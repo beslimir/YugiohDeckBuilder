10051f7d79d749690e41e3795e61f40a8b09daa1
beslimir <marin.beslo@gmail.com>
2022-01-22 16:29:49 +0100

    prepare for "add card to deck" feature

--- app/src/main/java/com/example/yugiohdeckbuilder/presentation/home_screen/components/YugiohCardRow.kt
+++ app/src/main/java/com/example/yugiohdeckbuilder/presentation/home_screen/components/YugiohCardRow.kt
@@ -1,20 +1,27 @@
 package com.example.yugiohdeckbuilder.presentation.home_screen.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.yugiohdeckbuilder.R
 import com.example.yugiohdeckbuilder.data.remote.dto.YugiohCard
 import com.example.yugiohdeckbuilder.presentation.home_screen.HomeScreenViewModel
 import com.example.yugiohdeckbuilder.presentation.ui.theme.Shapes
@@ -54,54 +61,41 @@ fun YugiohCardRow(
             }
     ) {
         Row(
-            modifier = Modifier.padding(4.dp)
+            modifier = Modifier.padding(4.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Column(
-                modifier = Modifier
-                    .height(70.dp)
-                    .width(50.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+            Row(
+                modifier = Modifier.weight(1f)
             ) {
-                GlideImage(
-                    imageModel = entries[index].cardImages!![0].imageUrlSmall,
-                    loading = {
-                        CircularProgressIndicator()
-                    }
-                )
-            }
+                Column(
+                    modifier = Modifier
+                        .height(70.dp)
+                        .width(50.dp),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    GlideImage(
+                        imageModel = entries[index].cardImages!![0].imageUrlSmall,
+                        loading = {
+                            CircularProgressIndicator()
+                        }
+                    )
+                }
 
-            Column(
-                verticalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier.padding(start = 8.dp)
-            ) {
-                Text(
-                    text = entries[index].name,
-                    style = TextStyle(
-                        fontSize = 12.sp,
-                        fontWeight = FontWeight.Bold
-                    )
-                )
-                Row {
+                Column(
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.padding(start = 8.dp)
+                ) {
                     Text(
-                        text = entries[index].type,
+                        text = entries[index].name,
                         style = TextStyle(
-                            fontSize = 10.sp,
-                            fontWeight = FontWeight.SemiBold
+                            fontSize = 12.sp,
+                            fontWeight = FontWeight.Bold
                         )
                     )
-                    Spacer(modifier = Modifier.width(10.dp))
-                    Text(
-                        text = entries[index].race,
-                        style = TextStyle(
-                            fontSize = 10.sp,
-                            fontWeight = FontWeight.SemiBold
-                        )
-                    )
-                    if (entries[index].type.contains("Monster")) {
-                        Spacer(modifier = Modifier.width(20.dp))
+                    Row {
                         Text(
-                            text = "ATK: ${entries[index].atk}",
+                            text = entries[index].type,
                             style = TextStyle(
                                 fontSize = 10.sp,
                                 fontWeight = FontWeight.SemiBold
@@ -109,28 +103,58 @@ fun YugiohCardRow(
                         )
                         Spacer(modifier = Modifier.width(10.dp))
                         Text(
-                            text = "DEF: ${entries[index].def}",
+                            text = entries[index].race,
                             style = TextStyle(
                                 fontSize = 10.sp,
                                 fontWeight = FontWeight.SemiBold
                             )
                         )
+                        if (entries[index].type.contains("Monster")) {
+                            Spacer(modifier = Modifier.width(20.dp))
+                            Text(
+                                text = "ATK: ${entries[index].atk}",
+                                style = TextStyle(
+                                    fontSize = 10.sp,
+                                    fontWeight = FontWeight.SemiBold
+                                )
+                            )
+                            Spacer(modifier = Modifier.width(10.dp))
+                            Text(
+                                text = "DEF: ${entries[index].def}",
+                                style = TextStyle(
+                                    fontSize = 10.sp,
+                                    fontWeight = FontWeight.SemiBold
+                                )
+                            )
+                        }
+                    }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        Text(
+                            text = entries[index].desc,
+                            style = TextStyle(
+                                fontSize = 10.sp
+                            ),
+                            maxLines = 3,
+                            overflow = TextOverflow.Ellipsis
+                        )
                     }
                 }
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
+            }
+
+                IconButton(
+                    onClick = {
+
+                    }
                 ) {
-                    Text(
-                        text = entries[index].desc,
-                        style = TextStyle(
-                            fontSize = 10.sp
-                        ),
-                        maxLines = 3,
-                        overflow = TextOverflow.Ellipsis
+                    Icon(
+                        imageVector = Icons.Default.CheckCircle,
+                        contentDescription = null
                     )
                 }
-            }
+            
         }
     }
 }
